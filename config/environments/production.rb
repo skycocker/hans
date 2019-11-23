0d1193e97ae7e2979feca14aa06240541a8fd440
Michał Siwek <mike21@aol.pl>
2019-11-23 21:36:31 +0100

    Prepare config for gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,19 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # gmail setup
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_options = {
+    from: Rails.application.secrets.gmail_username,
+  }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['GMAIL_LOGIN'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+  }
   # default url (e.g. for devise)
   config.action_mailer.default_url_options = { host: ENV['HOST'], port: ENV['PORT'] }
 
